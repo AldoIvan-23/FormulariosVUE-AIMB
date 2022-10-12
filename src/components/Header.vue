af22478e45f2e76b244ec5ed3d4e2bd59f678af7
a3521110330@alumno.uttehuacan.edu.mx <a3521110330@alumno.uttehuacan.edu.mx>
2022-10-12 13:30:48 -0500

    Subir proyecto completo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,32 @@
 <template>
     <div class="row">
-        <div class="col-12 col-md-4">
-            <h1>App - Proyectos</h1>
-        </div>
-        <div class="col-12 col-md-6">
-            <img width="100" :src="url" :alt="des">
-        </div>
+      <div class="col-12 col-md-6">
+        <h1>App - Proyectos</h1>
+      
+      </div>
+      <div class="col-12 col-md-6">
+        <img width="100" :src="url" :alt="des">
+      </div>
+      <hr>
     </div>
-    <hr />
 </template>
-
 <script>
-    export default {
-        data: () => ({
-            url: "https://cdn.svgporn.com/logos/vue.svg?response-content-disposition=attachment%3Bfilename%3Dvue.svg",
-            des: "Vue.js",
-        }),
-    };
+
+export default{
+  data:()=>({
+    url:"https://cdn.svgporn.com/logos/vue.svg",
+    des:"Vue.Js",
+  }),
+};
+
 </script>
+<style>
+@font-face {
+font-family: 'Oswald', sans-serif;
+src: url("https://fonts.gstatic.com");
+}
+h1{
+font-family: Oswald;
+font-variant: small-caps;
+}
+</style>
